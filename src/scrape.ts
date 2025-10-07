f527d2e51221cd4d77f643fcbeab1e94fedc5d03
vwkd <33468089+vwkd@users.noreply.github.com>
2025-10-07 20:26:17 +0200

    src/scrape: use Deno KV

--- src/scrape.ts
+++ src/scrape.ts
@@ -12,7 +12,8 @@ const STORE_ID_MIN = 1000;
 const STORE_ID_MAX = 9999;
 const OFFERS_URL =
   "https://www.netto-online.de/ueber-netto/Online-Prospekte.chtm";
-const CACHED_STORES_FILENAME = "cached_stores.jsonl";
+const CACHED_STORES_FILENAME = "cached_stores.db";
+const CACHED_STORES_PREFIX = ["netto-stores"];
 
 /**
  * Offer types
@@ -86,29 +87,16 @@ export async function scrape(options: Options) {
 
   console.info(`Scraping local offers for Netto stores...`);
 
+  await Deno.mkdir(dir, { recursive: true });
+
   const cachedStoresFilepath = join(dir, CACHED_STORES_FILENAME);
-
-  let cachedStores: Store[] = [];
-  try {
-    const cachedStoresJsonl = await Deno.readTextFile(cachedStoresFilepath);
-
-    cachedStores = cachedStoresJsonl
-      .split("\n")
-      .filter((line) => line.trim())
-      .filter(Boolean)
-      .map((line) => JSON.parse(line) as Store);
-  } catch (err) {
-    if (err instanceof Deno.errors.NotFound) {
-      // noop
-    } else {
-      throw err;
-    }
-  }
+  using kv = await Deno.openKv(cachedStoresFilepath);
+  const cachedStores =
+    (await Array.fromAsync(kv.list<Store>({ prefix: CACHED_STORES_PREFIX })))
+      .map((e) => e.value);
 
   const generalPageHtml = await getPage(OFFERS_URL);
   const generalBrochures = parseOfferPage(generalPageHtml).brochures;
-
-  await Deno.mkdir(dir, { recursive: true });
 
   const allStoreIds = new Set(range(STORE_ID_MIN, STORE_ID_MAX));
   const cachedStoreIds = new Set(cachedStores.map((s) => s.id));
@@ -158,13 +146,8 @@ export async function scrape(options: Options) {
         address: pageBrochures.address,
       };
 
-      await Deno.writeTextFile(
-        cachedStoresFilepath,
-        JSON.stringify(store) + "\n",
-        {
-          append: true,
-        },
-      );
+      const key = [...CACHED_STORES_PREFIX, id];
+      await kv.set(key, store);
     }
 
     // skip non-existing stores
